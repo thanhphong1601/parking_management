fde776953eb4714ac790cabc62a4eee08f446d5c
thanhphong1601 <phongmauser440@gmail.com>
2024-09-19 11:46:57 +0700

    start project

---
+++ parkingmanagementapp/src/main/java/com/phong/parkingmanagementapp/controllers/TestController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.phong.parkingmanagementapp.controllers;
+
+/**
+ *
+ * @author Admin
+ */
+public class TestController {
+    
+}
